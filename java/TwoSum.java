48a55729c94dedd0113823a83187e74db716564a
olivia zhang <oliviazhang0809@gmail.com>
2015-01-04 15:25:35 -0800

    update twosum

--- java/TwoSum.java
+++ java/TwoSum.java
@@ -1,9 +1,11 @@
 import java.util.HashMap;
-
+/**
+* Given an array of integers, find two numbers such that they add up to a specific target number.
+*/
 public class TwoSum {
+	
 	public static void main(String[] args) {
 		int[] numbers = { 0, 4, 3, 0 };
-
 		int[] out = twoSum(numbers, 0);
 		for (int i : out) {
 			System.out.print(i + " ");
@@ -16,21 +18,22 @@ public class TwoSum {
 		}
 		int[] res = new int[2];
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		// init
+		// put all numbers in array into a hashmap key: number, value: index
 		for (int i = 0; i < numbers.length; i++) {
 			map.put(numbers[i], i + 1);
 		}
-		System.out.println(map);
+
 		for (int i = 0; i < numbers.length; i++) {
+			// see if sum can be found
 			if (map.containsKey(target - numbers[i])) {
 				int index1 = i + 1;
 				int index2 = map.get(target - numbers[i]);
-				if (index1 == index2) {
-					continue;
+				// they cannot be same number
+				if (index1 != index2) {
+					res[0] = index1;
+					res[1] = index2;
+					return res;
 				}
-				res[0] = index1;
-				res[1] = index2;
-				return res;
 			}
 		}
 		return res;
